40155edeff85e545f19579819c548160c856af26
bbscoding <batuhan5534@gmail.com>
2026-01-02 18:09:07 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import QuizLogo from '../assets/quiz-logo.png'
+
+const Header = () => {
+  return (
+    <header>
+        <img src={QuizLogo} alt="Quiz logo" />
+        <h1>ReactQuiz</h1>
+    </header>
+  )
+}
+
+export default Header
